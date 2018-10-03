736f8cacab400ff0f3d74b92d64c298ee9f47173
Craig Da Silva <craig_344@hotmail.com>
2018-10-03 14:02:52 +0530

    OS memory

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const path = require('path');
+const os = require('os');
 
-var pathObj = path.parse(__filename);
+var totalMem = os.totalmem();
+var freeMem = os.freemem();
 
-console.log(pathObj);
+console.log(totalMem);
+console.log(freeMem);
