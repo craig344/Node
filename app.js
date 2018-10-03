7712092589b55c8c7ca2ae2f4fad697cdcb441de
Craig Da Silva <craig_344@hotmail.com>
2018-10-03 15:05:18 +0530

    Modular events

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(){
-    console.log("Message logged");
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
+    console.log("Message logged ",arg);
 })
 
-emitter.emit('messageLogged');
+logger.log('message');
